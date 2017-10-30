eba9da7b96ef0d6751a5c684614e47ea816b5416
AnnaBelskaya <hannah.belskaya@gmail.com>
2017-10-30 17:28:21 +0200

    The tasks 1-6 are done.

--- src/main/resources/2.sql
+++ src/main/resources/2.sql
@@ -1,7 +1,14 @@
 # Найти самый дорогой проект (исходя из ЗП разработчиков)
 
-SELECT projects.project_id, project_name, cost, company_id
-FROM projects
-GROUP BY cost
-ORDER BY cost DESC
+SELECT dev_projects.project_id, projects.project_name, projects.company_id,
+  companies.name, SUM(salary) as total_cost
+FROM dev_projects
+  JOIN developers
+    ON dev_projects.developer_id = developers.developer_id
+  JOIN projects
+    ON dev_projects.project_id = projects.project_id
+  JOIN companies
+    ON projects.company_id = companies.company_id
+GROUP BY dev_projects.project_id
+ORDER BY total_cost DESC
 LIMIT 1;
