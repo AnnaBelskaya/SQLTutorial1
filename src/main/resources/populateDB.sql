9f04906fbd2ddb285ae07a7f2d2c16a9460299e8
AnnaBelskaya <hannah.belskaya@gmail.com>
2017-10-31 21:15:54 +0200

    last commit

--- src/main/resources/populateDB.sql
+++ src/main/resources/populateDB.sql
@@ -1,397 +1,130 @@
-INSERT INTO companies (name,country,emloyees) VALUES (
-  'pineApple', 'USA', 23425
-);
-INSERT INTO companies(name,country,emloyees)  VALUES (
-  'Microsoft Doors', 'USA', 18234
-);
-INSERT INTO companies(name,country,emloyees)  VALUES (
-  'Orc Corporation', 'Mordor', 723
-);
-INSERT INTO companies(name,country,emloyees)  VALUES (
-  'HPotter', 'UK', 345
-);
-INSERT INTO companies (name,country,emloyees) VALUES (
-  'Forever Alone', 'Mars', 1
-);
-
+INSERT INTO companies (name,country,emloyees) VALUES
+  ('pineApple', 'USA', 23425),
+  ('Microsoft Doors', 'USA', 18234),
+  ('Orc Corporation', 'Mordor', 723),
+  ('HPotter', 'UK', 345),
+  ('Forever Alone', 'Mars', 1);
 
 INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Veniamin', 'Bublikov'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Anatoly', 'Novoseltsev'
-);
+VALUES
+  ('Veniamin', 'Bublikov'),
+  ('Anatoly', 'Novoseltsev'),
+  ('Lavrentiy', 'Pyatochkin'),
+  ('Foma', 'Shyshkin'),
+  ('Arsenij', 'Slivkin'),
+  ('Anton', 'Kartoshkin'),
+  ('Pierre', 'Bezukhov'),
+  ('Rodion', 'Raskolnikov'),
+  ('Nikolay', 'Kapustin'),
+  ('Timur', 'Kolobkov'),
+  ('Lukyan', 'Bobrikov'),
+  ('Gavrila', 'Smetanin');
 
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Lavrentiy', 'Pyatochkin'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Foma', 'Shyshkin'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Arsenij', 'Slivkin'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Anton', 'Kartoshkin'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Pierre', 'Bezukhov'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Rodion', 'Raskolnikov'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Nikolay', 'Kapustin'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Timur', 'Kolobkov'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Lukyan', 'Bobrikov'
-);
-INSERT INTO customers (customer_first_name, customer_last_name)
-VALUES (
-  'Gavrila', 'Smetanin'
-);
-
-
-INSERT INTO skills (skill_name) VALUES (
-  'Java'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'SQL'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'C++'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'HTML/CSS'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'UI/UX'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'JavaScript'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'C#'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'JDBC'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'Angular'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'Assembler'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'PHP'
-);
-INSERT INTO skills (skill_name) VALUES (
-  'Python'
-);
-
+INSERT INTO skills (skill_name) VALUES
+  ('Java'),
+  ('SQL'),
+  ('C++'),
+  ('HTML/CSS'),
+  ('UI/UX'),
+  ('JavaScript'),
+  ('C#'),
+  ('JDBC'),
+  ('Angular'),
+  ('Assembler'),
+  ('PHP'),
+  ('Python');
 
 INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Jack', 'Pepper', 10, 1
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Harry', 'Campbell', 2,1
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Charlie', 'Gray', 2,1
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Benjamin', 'Wood', 4,1
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Andy','Weir',7,5
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Nathaniel','Bean',14,4
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Christopher','Wells',34,4
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Louis','Hawkins', 20, 4
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Aiden','Mills', 2, 4
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Kevin','Nash',3,2
-);
-INSERT INTO developers(first_name, last_name, experience, company_id)
-VALUES (
-  'Andres','Bowen',23,2
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Kevin','Weaver',1,2
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Anthony','John',10,3
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Brandon','Williamson',3,3
-);
-INSERT INTO developers (first_name, last_name, experience, company_id)
-VALUES (
-  'Paul','Murphy',1,1
-);
-
+VALUES
+  ('Jack', 'Pepper', 10, 1),
+  ('Harry', 'Campbell', 2,1),
+  ('Charlie', 'Gray', 2,1),
+  ('Benjamin', 'Wood', 4,1),
+  ('Andy','Weir',7,5),
+  ('Nathaniel','Bean',14,4),
+  ('Christopher','Wells',34,4),
+  ('Louis','Hawkins', 20, 4),
+  ('Aiden','Mills', 2, 4),
+  ('Kevin','Nash',3,2),
+  ('Andres','Bowen',23,2),
+  ('Kevin','Weaver',1,2),
+  ('Anthony','John',10,3),
+  ('Brandon','Williamson',3,3),
+  ('Paul','Murphy',1,1);
 
 INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'ZeroOne', 1,4
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'Mars Online', 4,2
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'NASA Mission',  2,5
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'BigBug', 1,2
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES(
-  'GreenMars', 4,5
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES(
-  'Instakilogram', 7,2
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'JavaGeeks', 6,3
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'Socks Store', 8,1
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'JavaGuru', 10,3
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'Lambda', 10,3
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'PythonDevs', 9,4
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'WeLoveSQL', 7,3
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'Devs Diary',7,1
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'Happy Coder', 2,3
-);
-INSERT INTO projects (project_name, customer_id, company_id)
-VALUES (
-  'AntiBug', 12,3
-);
+VALUES
+  ('ZeroOne', 1,4),
+  ('Mars Online', 4,2),
+  ('NASA Mission',  2,5),
+  ('BigBug', 1,2),
+  ('GreenMars', 4,5),
+  ('Instakilogram', 7,2),
+  ('JavaGeeks', 6,3),
+  ('Socks Store', 8,1),
+  ('JavaGuru', 10,3),
+  ('Lambda', 10,3),
+  ('PythonDevs', 9,4),
+  ('WeLoveSQL', 7,3),
+  ('Devs Diary',7,1),
+  ('Happy Coder', 2,3),
+  ('AntiBug', 12,3);
 
+INSERT INTO dev_skills VALUES
+  (1, 1),
+  (1, 5),
+  (1, 2),
+  (2, 1),
+  (2, 8),
+  (3,  4),
+  (4, 2),
+  (4, 8),
+  (4, 9),
+  (5, 10),
+  (5, 1),
+  (6,  4),
+  (6,  5),
+  (6,  6),
+  (7, 12),
+  (7, 1),
+  (7, 2),
+  (8, 11),
+  (8, 3),
+  (9, 7),
+  (9, 9),
+  (10, 10),
+  (10, 3),
+  (10, 12),
+  (12, 2),
+  (13, 3),
+  (14, 1),
+  (15, 7),
+  (15, 10),
+  (15, 11);
 
-INSERT INTO dev_skills VALUES (
-  1, 1
-);
-INSERT INTO dev_skills VALUES (
-  1, 5
-);
-INSERT INTO dev_skills VALUES (
-  1, 2
-);
-INSERT INTO dev_skills VALUES (
-  2, 1
-);
-INSERT INTO dev_skills VALUES (
-  2, 8
-);
-INSERT INTO dev_skills VALUES (
-  3,  4
-);
-INSERT INTO dev_skills VALUES (
-  4, 2
-);
-INSERT INTO dev_skills VALUES (
-  4, 8
-);
-INSERT INTO dev_skills VALUES (
-  4, 9
-);
-INSERT INTO dev_skills VALUES (
-  5, 10
-);
-INSERT INTO dev_skills VALUES (
-  5, 1
-);
-INSERT INTO dev_skills VALUES (
-  6,  4
-);
-INSERT INTO dev_skills VALUES (
-  6,  5
-);
-INSERT INTO dev_skills VALUES (
-  6,  6
-);
-INSERT INTO dev_skills VALUES (
-  7, 12
-);
-INSERT INTO dev_skills VALUES (
-  7, 1
-);
-INSERT INTO dev_skills VALUES (
-  7, 2
-);
-INSERT INTO dev_skills VALUES (
-  8, 11
-);
-INSERT INTO dev_skills VALUES (
-  8, 3
-);
-INSERT INTO dev_skills VALUES (
-  9, 7
-);
-INSERT INTO dev_skills VALUES (
-  9, 9
-);
-INSERT INTO dev_skills VALUES (
-  10, 10
-);
-INSERT INTO dev_skills VALUES (
-  10, 3
-);
-INSERT INTO dev_skills VALUES (
-  10, 12
-);
-INSERT INTO dev_skills VALUES (
-  12, 2
-);
-INSERT INTO dev_skills VALUES (
-  13, 3
-);
-INSERT INTO dev_skills VALUES (
-  14, 1
-);
-INSERT INTO dev_skills VALUES (
-  15, 7
-);
-INSERT INTO dev_skills VALUES (
-  15, 10
-);
-INSERT INTO dev_skills VALUES (
-  15, 11
-);
-
-
-INSERT INTO dev_projects VALUES (
-  1, 6
-);
-INSERT INTO dev_projects VALUES (
-  1, 7
-);
-INSERT INTO dev_projects VALUES (
-  1, 8
-);
-INSERT INTO dev_projects VALUES (
-  2, 11
-);
-INSERT INTO dev_projects VALUES (
-  2, 12
-);
-INSERT INTO dev_projects VALUES (
-  3,  5
-);
-INSERT INTO dev_projects VALUES (
-  4, 10
-);
-INSERT INTO dev_projects VALUES (
-  4, 12
-);
-INSERT INTO dev_projects VALUES (
-  5, 5
-);
-INSERT INTO dev_projects VALUES (
-  6, 10
-);
-INSERT INTO dev_projects VALUES (
-  6, 11
-);
-INSERT INTO dev_projects VALUES (
-  7, 14
-);
-INSERT INTO dev_projects VALUES (
-  8, 3
-);
-INSERT INTO dev_projects VALUES (
-  8, 4
-);
-INSERT INTO dev_projects VALUES (
-  9, 13
-);
-INSERT INTO dev_projects VALUES (
-  9, 14
-);
-INSERT INTO dev_projects VALUES (
-  10, 14
-);
-INSERT INTO dev_projects VALUES (
-  11, 6
-);
-INSERT INTO dev_projects VALUES (
-  11, 7
-);
-INSERT INTO dev_projects VALUES (
-  12, 14
-);
-INSERT INTO dev_projects VALUES (
-  13, 15
-);
-INSERT INTO dev_projects VALUES (
-  13, 1
-);
-INSERT INTO dev_projects VALUES (
-  14, 13
-);
-INSERT INTO dev_projects VALUES (
-  14, 14
-);
-INSERT INTO dev_projects VALUES (
-  15,14
-);
+INSERT INTO dev_projects VALUES
+  (1, 6),
+  (1, 7),
+  (1, 8),
+  (2, 11),
+  (2, 12),
+  (3,  5),
+  (4, 10),
+  (4, 12),
+  (5, 5),
+  (6, 10),
+  (6, 11),
+  (7, 14),
+  (8, 3),
+  (8, 4),
+  (9, 13),
+  (9, 14),
+  (10, 14),
+  (11, 6),
+  (11, 7),
+  (12, 14),
+  (13, 15),
+  (13, 1),
+  (14, 13),
+  (14, 14),
+  (15,14);
